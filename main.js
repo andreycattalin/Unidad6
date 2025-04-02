2591b9ad5f7b034eea12aeda84aeac7d830d4118
andrey <andrey@sublimestudio.es>
2025-04-02 20:54:51 +0200

    cosas

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const divPrincipal = document.getElementById("gatitos");
 
-divPrincipal.innerHTML += `<img src="https://cdn2.thecatapi.com/images/gi.jpg" alt="" srcset="">`
-divPrincipal.innerHTML += `<img src="https://cdn2.thecatapi.com/images/gi.jpg" alt="" srcset="">`
-divPrincipal.innerHTML += `<img src="https://cdn2.thecatapi.com/images/gi.jpg" alt="" srcset="">`
+
+function mostrarGatitos() {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        data.forEach(gato => {
+            divPrincipal.innerHTML += `<img src="${gato.url}" alt="" srcset="">`
+        });
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+mostrarGatitos();
