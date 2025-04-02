edc9dbb54d55de2f2ef81886faa9fd1a895eb483
andrey <andrey@sublimestudio.es>
2025-04-02 20:47:15 +0200

    otro va

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+const divPrincipal = document.getElementById("gatitos");
+
+divPrincipal.innerHTML += `<img src="https://cdn2.thecatapi.com/images/gi.jpg" alt="" srcset="">`
+divPrincipal.innerHTML += `<img src="https://cdn2.thecatapi.com/images/gi.jpg" alt="" srcset="">`
+divPrincipal.innerHTML += `<img src="https://cdn2.thecatapi.com/images/gi.jpg" alt="" srcset="">`
